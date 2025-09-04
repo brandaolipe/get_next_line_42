5a79537d49e05c0eeb6573dca8956cc5bda7feb8
febranda <felipebrandaochaves@gmail.com>
2025-09-04 19:16:12 -0300

    create node struct

--- get_next_line.h
+++ get_next_line.h
@@ -3,17 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: febranda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 17:02:16 by febranda          #+#    #+#             */
-/*   Updated: 2025/09/02 17:13:37 by febranda         ###   ########.fr       */
+/*   Updated: 2025/09/04 19:14:43 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <fcntl.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+typedef struct s_node
+{
+	char			*str;
+	struct s_node	*next;
+}t_node;
 
 #endif
