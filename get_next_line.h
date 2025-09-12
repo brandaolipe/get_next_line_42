b7afc65e6f4b6d5805eee99b9b61d3f808e06a19
febranda <felipebrandaochaves@gmail.com>
2025-09-12 19:18:59 -0300

    .

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 17:02:16 by febranda          #+#    #+#             */
-/*   Updated: 2025/09/09 19:49:40 by febranda         ###   ########.fr       */
+/*   Updated: 2025/09/12 17:54:37 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,12 @@ typedef struct s_list
 	char			*content;
 	struct s_list	*next;
 }t_list;
+
+char	*get_next_line(int fd);
+void	fill_list(t_list **lst, int fd);
+void	add_node(t_list **lst, char *buffer);
+int		detect_newline(t_list *lst);
+char	*take_newline(t_list *lst);
+int		line_length(t_list *lst);
 
 #endif
